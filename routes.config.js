29582a4dbe9b3a75fe67078f8b1263550a348390
shischiff <50595228+shischiff@users.noreply.github.com>
2020-05-04 20:01:02 +0300

    adding logger (log file, timestamp and context uuid) and better user messaging (#3)
    Co-authored-by: Shimron Schiff <sschiff@redhat.com>

--- routes.config.js
+++ routes.config.js
@@ -1,13 +1,14 @@
 const { clientValidationRules, validate } = require('./lib/validator.js')
 const send2multipleSSOs = require('./lib/multiple_sso')
 const generator = require('generate-password');
+// const { v4: uuidv4 } = require('uuid');
 
 request = require('request');
 
 let mode = process.env.MODE || "prod";
 const config = require('./config/' + mode + '.env.config.js');
 
-exports.routesConfig = function (app) {
+exports.routesConfig = function (app, logger) {
     app.get("/", function(req, res) {
                      res.render('index', { message: ' '});
                  });
@@ -15,13 +16,14 @@ exports.routesConfig = function (app) {
                      res.render('index', { message: ' '});
                  });
     app.post("/new_client", clientValidationRules(), validate, (req, res) => {
-      const client = req.body;
+        let uuid= req.id;
+        const client = req.body;
 
-        if (client.webForm) { //redirect uris recieved via web form are separated by new lines, this will convert it to an array of URIs
-            console.log('Recieved Webform: ' );
+        if (client.webForm) { //redirect uris received via web form are separated by new lines, this will convert it to an array of URIs
+            logger.info('Received Webform: ', {"uuid": uuid} );
             client.redirectUris = client.redirectUris.split(",");
         } else{
-            console.log('Recieved API call: ' );
+            logger.info('Received API call: ', {"uuid": uuid} );
         }
         client.description={ //creating a JSON object from the contact details provided in the webform
         "contactName": client.contactName,
@@ -40,7 +42,7 @@ exports.routesConfig = function (app) {
             });
         }
 
-        body = {
+        let body = {
             "clientId": client.id,
             "secret": client.secret,
             "redirectUris": client.redirectUris,
@@ -48,39 +50,48 @@ exports.routesConfig = function (app) {
             // "rootUrl": client.rootUrl,
             "name": client.name
         };
-        console.log();
-        console.dir(body);
+        logger.info("Received body: ", {"uuid": uuid, "body": body});
 
-
-
-        send2multipleSSOs.createClient(null, body,function(error, ssoResponses) {
+        send2multipleSSOs.createClient(null, body, uuid, logger,function(error, ssoResponses) {
             /*  example ssoResponses: [{"name": "sso-1","code": 200,"msg": "client created successfully"},{"name": "sso-2","code": 400,"msg": "Bad Request"}]   */
             // process the respons(es) from RHSSO
             let responsesProcessed=0;
-            let webMessage = "| ";
             let apiResponseCode=201;
-            let apiMessage = "| ";
-
+            let resMessage = "";
+            let errorCode =  "";
             if (error) {
-                    console.log('error:' + error);
-                    if (client.webForm) { // request origination is from a web form:
-                        webMessage = "Something went wrong server error";
-                        res.render('error', {message: webMessage});
+                errorCode = "e1001";
+                logger.error('error:' + error, {"uuid": uuid, "errorCode": errorCode});
+                resMessage = "Something went wrong - server error " + errorCode;
+                if (client.webForm) { // request origination is from a web form:
+                        res.render('error', {message: resMessage});
                     } else {            //request origination is from a direct API call:
-                        console.log("api response: " + apiMessage + webMessage);
+                        logger.info("api response: " + resMessage, {"uuid": uuid,"errorCode": errorCode});
                         res.status(apiResponseCode).json({
-                            message: apiMessage + webMessage
+                            message: resMessage
                         });
                     }
-
             }
-            else {
-                if (ssoResponses.map(x => x.code).includes(400)) { // one or more response failed, then run cleanup:
-                    console.log("One or more endpoints failed to create the client " + body.clientId + ". Checking if clean up is required... ")
+            else { // not error
+                if (ssoResponses.map(x => x.code).every(code => code === 201)  ){ // *All* endpoints succeeded:
+                    apiResponseCode = 201;
+                    resMessage = "Client created successfully.|Take note of the following details:|ClientID = " + body.clientId + "|Secret = " + body.secret + "|redirect URIs: " + body.redirectUris+  "|These will only display once, copy these values and use them in your app";
+                    if (client.webForm) { // request origination is from a web form:
+                        logger.info("Web response: " + resMessage, {"uuid": uuid});
+                        resMessage = resMessage.split('|').join('<br>');
+                        res.render('response', {message: resMessage});
+                    } else {            //request origination is from a direct API call:
+                        logger.info("api response: " + resMessage, {"uuid": uuid});
+                        res.status(apiResponseCode).json({
+                            message: resMessage
+                        });
+                    }
+                } else { // one or more response failed, then run cleanup:
+                    logger.info("One or more endpoints failed to create the client " + body.clientId + ". Checking if clean up is required... ", {"uuid": uuid})
                     // Iterate over the responses and check, if succeeded use its token to delete it :
                     ssoResponses.forEach(function (ssoRes) {
                         if (ssoRes.code === 201) { //this client was created, clean it up:
-                            console.log("starting clean up of client: " + body.clientId + " on rhsso: " + ssoRes.name);
+                            logger.info("starting clean up of client: " + body.clientId + " on rhsso: " + ssoRes.name, {"uuid": uuid});
                             let token = 'bearer ' + ssoRes.registrationAccessToken;
                             request.delete(
                                 {
@@ -92,62 +103,70 @@ exports.routesConfig = function (app) {
                                 },
                                 function (error,response){
                                     if (error) { //if delete request failed
-                                        console.log('Clean up failed, could not delete the client from: ' + ssoRes.name + ' error message: ' + error);
+                                        errorCode = "e1002";
+                                        logger.error('Clean up failed, could not delete the client from: ' + ssoRes.name + ' error message: ' + error, {"uuid": uuid, "errorCode": errorCode});
                                         apiResponseCode=500;
                                         responsesProcessed += 1;
-                                    }
-                                    if (response.statusCode !== 204) { //Deletion failed:
-                                        console.log('Cleanup failed, could not delete the client from: ' + ssoRes.name + ' error message: ' + response.body.error_description);
-                                        apiResponseCode=500;
-                                        responsesProcessed += 1;
-                                    } else { //Clean up was successful
-                                        console.log('Cleanup done. sso: ' + ssoRes.name + " clientId: " + body.clientId + " deleted successfully");
-                                        apiResponseCode=400;
-                                        webMessage += ssoRes.name + " - message: Cleanup performed due to failure of another site | ";
-                                        responsesProcessed += 1;
-                                    }
-                                    if(responsesProcessed === ssoResponses.length) { // respond to the user with a summary of all sites:
-                                        if (client.webForm) { // request origination is from a web form:
-                                            if (apiResponseCode === 500) {
-                                                res.render('error', {message: "Server error - Something went wrong"});
-                                            }else {
-                                                webMessage = webMessage.split('|').join('<br>');
-                                                res.render('response', {message: webMessage});
+                                    }else {
+                                        if (response.statusCode !== 204) { //Deletion failed:
+                                            errorCode = "e1003";
+                                            logger.error('Cleanup failed, could not delete the client from: ' + ssoRes.name + ' error message: ' + response.body.error_description, {"uuid": uuid, "errorCode": errorCode});
+                                            apiResponseCode = 500;
+                                            responsesProcessed += 1;
+                                        } else { //Clean up was successful
+                                            logger.info('Cleanup done. sso: ' + ssoRes.name + " clientId: " + body.clientId + " deleted successfully", {"uuid": uuid});
+                                            apiResponseCode = 400;
+                                            responsesProcessed += 1;
+                                        }
+                                        if (responsesProcessed === ssoResponses.length) { // if Finished iterating, respond to the user with a summary of all sites:
+                                            if (client.webForm) { // request origination is from a web form:
+                                                if (apiResponseCode === 500) {
+                                                    res.render('error', {message: "Server error - Something went wrong. error: " + errorCode});
+                                                } else { // Clean up was successful
+                                                    if(ssoResponses.map(x => x.msg).includes("Client Identifier in use")){
+                                                        res.render('response', {message: "Failed to create client - Client Id already in use"});
+                                                    }else{
+                                                        // errorCode =
+                                                        res.render('response', {message: "Failed to create client. error: " + errorCode});
+                                                    }
+                                                }
+                                            } else {            //request origination is from a direct API call:
+                                                resMessage = "Failed to create client";
+                                                logger.info("api response: " + resMessage, {"uuid": uuid});
+                                                res.status(apiResponseCode).json({
+                                                    message:  resMessage
+                                                });
                                             }
-                                        } else {            //request origination is from a direct API call:
-                                            console.log("api response: " + apiMessage + webMessage);
-                                            res.status(apiResponseCode).json({
-                                                message: apiMessage + webMessage
-                                            });
                                         }
                                     }
                                 })
                         } else { // This client failed to create:
-                            webMessage += ssoRes.name + " - message: " + ssoRes.msg + ". code: " + ssoRes.code + " | ";
+                            errorCode += ssoRes.errorCode;
                             apiResponseCode = 400;
                             responsesProcessed += 1;
                         }
                     })
-                } else { // All endpoints succeeded:
-                    ssoResponses.forEach(function (ssoRes, index) {
-                        webMessage += ssoRes.name + ": " + ssoRes.msg + " | ClientID = " + body.clientId + " Secret = " + ssoRes.secret + " | This will only display once, copy these values and use them in your app";
-                        apiResponseCode = 201;
-                        apiMessage = "All clients were created successfully. ClientID = " + body.clientId + " Secret = " + ssoRes.secret + " | This will only display once, copy these values and use them in your app";
-                        responsesProcessed += 1;
-                    });
                 }
-                if(responsesProcessed === ssoResponses.length) { // respond to the user with a summary of all sites:
+                if (responsesProcessed === ssoResponses.length) { // if Finished iterating, respond to the user with a summary of all sites:
                     if (client.webForm) { // request origination is from a web form:
-                        webMessage = webMessage.split('|').join('<br>');
-                        res.render('response', {message: webMessage});
+                        if (apiResponseCode === 500) {
+                            res.render('error', {message: "Server error - Something went wrong. error: " + errorCode});
+                        } else { // Clean up was successful
+                            if(ssoResponses.map(x => x.msg).includes("Client Identifier in use")){
+                                res.render('response', {message: "Failed to create client - Client Id already in use"});
+                            }else{
+                                res.render('response', {message: "Failed to create client. error: " + errorCode});
+                            }
+                        }
                     } else {            //request origination is from a direct API call:
-                        console.log("api response: " + apiMessage + webMessage);
+                        resMessage = "Failed to create client";
+                        logger.info("api response: " + resMessage, {"uuid": uuid});
                         res.status(apiResponseCode).json({
-                            message: apiMessage + webMessage
+                            message:  resMessage
                         });
                     }
                 }
             }
         })
     });
-}
+};
